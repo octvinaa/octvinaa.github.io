bbd3a0096199fe1ecfc873453f1956f6d96483eb
Rifqi Favian Hibatullah <108116370+rifqiFavian463@users.noreply.github.com>
2024-09-13 22:39:17 +0700

    style(dashboard): change css in mobile responsive

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -40,10 +40,10 @@ function Dashboard() {
       <div className="flex gap-3 items-center">
         <PiCodeThin className="text-6xl text-neutral-50 min-w-40 md:w-fit" />
         <div className="flex flex-col max-w-80 gap-1">
-          <span className="text-lg sm:text-xl font-extrabold text-neutral-300">
-            Portfolio, <span className="text-[#dcfc35] text-base">A Front-end Journey</span>
+          <span className="text-[.9rem] md:text-lg sm:text-xl font-extrabold text-neutral-300">
+            Portfolio, <span className="text-[#dcfc35] text-[.9rem]">A Front-end Journey</span>
           </span>
-          <span className="text-sm text-neutral-400">Check out my portfolio and explore the projects where I have crafted seamless frontend experiences!.</span>
+          <span className="text-[.8rem] text-neutral-400">Check out my portfolio and explore the projects where I have crafted seamless frontend experiences!.</span>
         </div>
       </div>
 
@@ -85,11 +85,11 @@ function Dashboard() {
               {portfolioData.map((item) => (
                 <Link key={item.id} href={`/dashboard/${item.id}`}>
                   <motion.div whileHover={{ backgroundColor: "#282828" }} className="flex items-center gap-x-3 p-4 rounded-md cursor-pointer">
-                    <Image src={item.thumbnail} alt="Portfolio" width={150} height={100} className="rounded-md w-[175px] object-contain " />
+                    <Image src={item.thumbnail} alt="Portfolio" width={150} height={100} className="rounded-md w-[120px] h-[100px] object-cover xl:w-[175px] " />
                     <motion.div className="flex flex-col">
-                      <motion.span className="text-sm text-neutral-400">{item.title}</motion.span>
-                      <motion.span className="text-xs font-bold text-neutral-400">{item.desc}</motion.span>
-                      <motion.span className="text-xs text-neutral-300 font-bold text-start mt-3">More</motion.span>
+                      <motion.span className="text-[.8rem] lg:text-[.7rem] xl:text-sm text-neutral-400">{item.title}</motion.span>
+                      <motion.span className="text-[.7rem] lg:text-[.6rem] xl:text-xs font-bold text-neutral-400">{item.desc}</motion.span>
+                      <motion.span className="text-[.7rem] lg:text-[.6rem] xl:text-xs  text-neutral-300 font-bold text-start mt-3">More</motion.span>
                     </motion.div>
                   </motion.div>
                 </Link>
@@ -108,10 +108,10 @@ function Dashboard() {
         {/* <GiPencilBrush className="text-5xl text-neutral-50 min-w-40 md:w-fit" /> */}
         <Image src="/illustration/illustration2.png" alt="Illustration Thumbnail" width={135} height={135} className="w-[135px] h-[135px] object-cover object-left-top rounded-md" />
         <div className="flex flex-col max-w-80 gap-1">
-          <span className="text-lg sm:text-xl font-extrabold text-neutral-300">
-            Gallery, <span className="text-[#dcfc35] text-base">A Illustrator Journey</span>
+          <span className="text-lg text-[.9rem] sm:text-xl font-extrabold text-neutral-300">
+            Gallery, <span className="text-[#dcfc35] text-[.9rem]">A Illustrator Journey</span>
           </span>
-          <span className="text-sm text-neutral-400">Explore my vibrant anime illustrations, capturing emotion and dynamic scenes with bold, expressive artistry.</span>
+          <span className="text-[.7rem] text-neutral-400">Explore my vibrant anime illustrations, capturing emotion and dynamic scenes with bold, expressive artistry.</span>
           <button type="button" className="bg-[#696969] text-xs text-neutral-50 h-7 w-full rounded-md mt-4">
             <Link href="dashboard/gallery">Explore Art</Link>
           </button>
